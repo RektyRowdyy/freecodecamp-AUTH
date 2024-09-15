bcf8af8e19c4f478afa172e51a05e8478148f690
RektyRowdyy <rektyrowdy@gmail.com>
2024-09-15 18:09:15 +0530

    add env in google strategy

--- API/src/strategies/google-strategy.mjs
+++ API/src/strategies/google-strategy.mjs
@@ -15,7 +15,7 @@ export default passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: `https://freecodecamp-api-blond.vercel.app/api/googleCallback`,
+            callbackURL: `${process.env.API_URL}/api/googleCallback`,
             scope: ["profile", "email"]
         },
         async (accessToken, refreshToken, profile, done) => {
